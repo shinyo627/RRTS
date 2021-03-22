1ee8c4b9d859f59520fa8dac611469b384ae3cc0
shinyo627 <shinyo627@gmail.com>
2021-03-22 11:54:26 +0100

    First Commit and did basic set up for redux, App component, reducers, action

---
+++ src/actions/types.ts
@@ -0,0 +1,6 @@
+export enum ActionTypes {
+  // The only requirement of a type in a redux action is that
+  // it must be some unique value in theory. EXAMPLE )A unique string
+  // In typescript, you just declare variable with names alone, it's assigned automatically with number from 0
+  fetchTodos, // === 0 in TS
+}
